d8b896002d11144bcf72907bcfbd51e41569fda9
clroyale <patrickjjarise@gmail.com>
2015-12-03 11:09:59 -0700

    removing moved files

--- public/js/constants.js
+++
@@ -1,7 +0,0 @@
-var constants = {
-	LOAD_PRODUCTS_SUCCESS: 'LOAD_PRODUCTS_SUCCESS',
-	LOAD_CATEGORIES_SUCCESS: 'LOAD_CATEGORIES_SUCCESS',
-	API_URL_DEV: 'http://localhost:3000/api/'
-};
-
-module.exports = constants;
